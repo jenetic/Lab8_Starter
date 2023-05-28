cffc22b9905a9133a6367fb4d8c9149e5b301ffc
jenetic <jznnylam@gmail.com>
2023-05-28 14:33:03 -0700

    Do lab

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+1. I would pick 1. This is so the tests are automated and always run when I decide to change the code or add a feature without me thinking to manually run them.
+
+2. No
+
+3. No. This is because the message feature requires user interaction, so it would be better to test it using end-to-end testing.
+
+4. Yes. This does not require user interaction and so we can just test the function that regulates the length of a message in isolation using unit testing.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,122 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('is phone number - correct', () => {
+    // TODO
+    const res = functions.isPhoneNumber("(423) 342-8932")
+    expect(res).toBe(true);
+});
+
+test('is phone number - correct', () => {
+    // TODO
+    const res = functions.isPhoneNumber("(000) 000-0000")
+    expect(res).toBe(true);
+});
+
+test('is phone number - incorrect', () => {
+// TODO
+    const res = functions.isPhoneNumber("(4233232342sas8932")
+    expect(res).toBe(false);
+});
+
+test('is phone number - incorrect', () => {
+    // TODO
+    const res = functions.isPhoneNumber("34")
+    expect(res).toBe(false);
+});
+
+test('isEmail - correct', () => {
+    // TODO
+    const res = functions.isEmail("isfdkj@gmail.com")
+    expect(res).toBe(true);
+});
+
+test('isEmail - correct', () => {
+    // TODO
+    const res = functions.isEmail("isfsfs@sdfsd.fsd")
+    expect(res).toBe(true);
+});
+
+test('isEmail - incorrect', () => {
+    // TODO
+    const res = functions.isEmail("isfsfdfsd.fsd")
+    expect(res).toBe(false);
+});
+
+test('isEmail - incorrect', () => {
+    // TODO
+    const res = functions.isEmail("isfsfs.fsd@sdsd")
+    expect(res).toBe(false);
+});
+
+test('isStrongPassword - correct', () => {
+    // TODO
+    const res = functions.isStrongPassword("password")
+    expect(res).toBe(true);
+});
+
+test('isStrongPassword - correct', () => {
+    // TODO
+    const res = functions.isStrongPassword("password123")
+    expect(res).toBe(true);
+});
+
+test('isStrongPassword - incorrect', () => {
+    // TODO
+    const res = functions.isStrongPassword("3Lsr|&9V<A|`)h0")
+    expect(res).toBe(false);
+});
+
+test('isStrongPassword - incorrect', () => {
+    // TODO
+    const res = functions.isStrongPassword("I_0oz25TO0R4nPSm?ek7~dJrJ$Y4")
+    expect(res).toBe(false);
+});
+
+test('isDate - correct', () => {
+    // TODO
+    const res = functions.isDate("99/94/0699")
+    expect(res).toBe(true);
+});
+
+test('isDate - correct', () => {
+    // TODO
+    const res = functions.isDate("00/00/0000")
+    expect(res).toBe(true);
+});
+
+test('isDate - incorrect', () => {
+    // TODO
+    const res = functions.isDate("05-12-2022")
+    expect(res).toBe(false);
+});
+
+test('isDate - incorrect', () => {
+    // TODO
+    const res = functions.isDate("06/2022")
+    expect(res).toBe(false);
+});
+
+test('isHexColor - correct', () => {
+    // TODO
+    const res = functions.isHexColor("#5c84ab")
+    expect(res).toBe(true);
+});
+
+test('isHexColor - correct', () => {
+    // TODO
+    const res = functions.isHexColor("5c84ab")
+    expect(res).toBe(true);
+});
+
+test('isHexColor - incorrect', () => {
+    // TODO
+    const res = functions.isHexColor("blue")
+    expect(res).toBe(false);
+});
+
+test('isHexColor - incorrect', () => {
+    // TODO
+    const res = functions.isHexColor("#jenny")
+    expect(res).toBe(false);
+});
